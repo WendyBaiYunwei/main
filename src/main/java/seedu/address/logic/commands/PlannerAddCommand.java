d0e6e0c651bb1de5f791cd3aea51990399bca948
Bai Yunwei <yunweibai@gmail.com>
2019-03-03 16:38:22 +0800

    Planner: amend planner add command

--- src/main/java/seedu/address/logic/commands/PlannerAddCommand.java
+++ src/main/java/seedu/address/logic/commands/PlannerAddCommand.java
@@ -17,7 +17,7 @@ import seedu.address.model.module.Module;
  */
 public class PlannerAddCommand extends Command {
 
-    public static final String COMMAND_WORD = "planner add";
+    public static final String COMMAND_WORD = "planneradd";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a module to the degree planner. "
             + "Parameters: "
