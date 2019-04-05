ff36f909652f60177a0d13849807de499effd71c
Bai Yunwei <yunweibai@gmail.com>
2019-04-05 13:12:16 +0800

    planner_suggest: add missing comment

--- src/main/java/pwe/planner/model/planner/SortModulesToSuggest.java
+++ src/main/java/pwe/planner/model/planner/SortModulesToSuggest.java
@@ -6,6 +6,13 @@ import java.util.Comparator;
  * Sorts according to the lowest credits difference if tie.
  */
 public class SortModulesToSuggest implements Comparator<ModuleToSuggest> {
+    /**
+     *
+     * @param moduleA A valid moduleA to suggest
+     * @param moduleB A valid moduleB to suggest
+     * @return number of matching tags difference between two modules to suggest, or
+     * credit difference between two modules if tie.
+     */
     public int compare(ModuleToSuggest moduleA, ModuleToSuggest moduleB) {
         if (moduleA.getNumberOfMatchingTags() == moduleB.getNumberOfMatchingTags()) {
             return moduleA.getCreditDifference() - moduleB.getCreditDifference();
