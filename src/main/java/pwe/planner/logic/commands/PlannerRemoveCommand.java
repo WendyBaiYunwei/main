f66da6a5fa710a58ab4dd630616d6d259171f30d
Bai Yunwei <yunweibai@gmail.com>
2019-04-14 12:55:19 +0800

    planner_remove: change message

--- src/main/java/pwe/planner/logic/commands/PlannerRemoveCommand.java
+++ src/main/java/pwe/planner/logic/commands/PlannerRemoveCommand.java
@@ -30,10 +30,11 @@ public class PlannerRemoveCommand extends Command {
             + "[" + PREFIX_CODE + "CODE]...\n"
             + "Example: " + COMMAND_WORD + " code/CS1010 code/CS2040C";
 
-    public static final String MESSAGE_SUCCESS = "Removed module(s) %1$s from the degree plan\n"
+    public static final String MESSAGE_SUCCESS = "Successfully removed module(s) %1$s from the degree plan!\n"
             + "Co-requisite(s) removed: %2$s";
-    public static final String MESSAGE_NONEXISTENT_CODES = "The module code(s) %1$s does not exist in the"
-            + " degree plan.";
+    public static final String MESSAGE_NONEXISTENT_CODES = "You cannot remove module(s) %1$s that does not"
+            + "exist in the degree plan.\n [Tip] Maybe you can review the module(s) in the degree plan, "
+            + "then select module(s) to remove from there.";
     private Set<Code> codesToRemove;
 
     /**
